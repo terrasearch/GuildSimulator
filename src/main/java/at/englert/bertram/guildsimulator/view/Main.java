92b030b0d0d67e96a101d955818c88190024eab8
Bertram Englert <bertram.englert@gmx.at>
2019-07-11 14:19:48 +0200

    Created config.properties and main

---
+++ src/main/java/at/englert/bertram/guildsimulator/view/Main.java
@@ -0,0 +1,10 @@
+package at.englert.bertram.guildsimulator.view;
+
+import de.gurkenlabs.litiengine.Game;
+
+public class Main {
+    public static void main(String[] args) {
+        Game.init(args);
+        Game.start();
+    }
+}
